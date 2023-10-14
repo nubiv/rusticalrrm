df169e7a9bc53916c83854c8c0aece105a9556b4
horus <h.horace0921@gmail.com>
2023-10-14 21:00:05 +0800

    fix: vercel build

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,7 +5,7 @@ import {
   type DefaultSession,
   type NextAuthOptions,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+// import DiscordProvider from "next-auth/providers/discord";
 import GoogleProvider from "next-auth/providers/google";
 
 import { env } from "@/env.mjs";
